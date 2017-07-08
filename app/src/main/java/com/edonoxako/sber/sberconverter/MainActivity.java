6a8d02df37efebc8d877403cafea5274ef867515
edonoxako <edonoxako@gmail.com>
2017-07-08 12:05:05 +0700

    Initial commit

---
+++ app/src/main/java/com/edonoxako/sber/sberconverter/MainActivity.java
@@ -0,0 +1,13 @@
+package com.edonoxako.sber.sberconverter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
