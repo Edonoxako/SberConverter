9cf03a0b730849e87855385c9d5553c9e4588918
edonoxako <edonoxako@gmail.com>
2017-07-08 12:19:52 +0700

    Added wiremock for http requests testing

--- app/src/test/java/com/edonoxako/sber/sberconverter/ExampleUnitTest.java
+++ app/src/test/java/com/edonoxako/sber/sberconverter/ExampleUnitTest.java
@@ -1,15 +1,25 @@
 package com.edonoxako.sber.sberconverter;
 
+import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
+
+import org.junit.Rule;
 import org.junit.Test;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 import static org.junit.Assert.*;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
+    @Rule
+    public WireMockRule wireMockRule = new WireMockRule(wireMockConfig()
+                                                            .dynamicPort()
+                                                            .dynamicHttpsPort());
+
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
